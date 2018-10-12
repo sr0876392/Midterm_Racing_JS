0bc61146e727838d356a40f36a38386d4ca35d92
debian <debian@debians-MacBook-Pro.local>
2018-10-12 10:32:06 -0500

    something different

--- Midterm_Project_JS/script.js
+++ Midterm_Project_JS/script.js
@@ -1,8 +1,12 @@
-const cars = {
-    carsDiv: document.getElementById("cars"),
-    picArr: ["<img src='pics/yellow_car.png'> ",
-             "<img src='pics/green_car.png'> ", "<img src='pics/black_car.png'> " ]
-};
+
+   var carsDiv = document.getElementById("cars");
+    
+const cars = [
+    
+    {pic: "<img src='pics/yellow_car.png'> "},
+    {pic: "<img src='pics/green_car.png'> "}, 
+    {pic: "<img src='pics/black_car.png'> "} 
+];
 const lights = {
     greenLight: document.getElementById("green"),
     redLight: document.getElementById("red"),
@@ -10,10 +14,10 @@ const lights = {
 }
 
 function fillCarsDiv() {
-    for (i = 0; i < cars.picArr.length; i++){
-        var createCarDiv = document.createElement("div"); 
-        createCarDiv.innerHTML += cars.picArr[i];
-        cars.carsDiv.appendChild(createCarDiv);
+    for (i = 0; i < cars.length; i++){
+        cars[i].element = document.createElement("div"); 
+        cars[i].element.innerHTML += cars[i].pic;
+        carsDiv.appendChild(cars[i].element);
     }
 }
 
@@ -21,21 +25,9 @@ function fillCarsDiv() {
 lights.startButton.addEventListener("click", function(){
     lights.redLight.style.backgroundColor = "grey";
     lights.greenLight.style.backgroundColor = "green";
-    elementChildren = document.getElementById("cars").children;
-    var carPos = {
-        width: 0,
-        step: 0,
-    }
-    var temp = elementChildren[1].getBoundingClientRect();
-    var rect = document.getElementById("finishLine").getBoundingClientRect();
-
-    while (rect.right > temp.right) {
-        x =+10;
-        elementChildren[1].marginLeft = `${x}px`
-    }  
-        console.log(carPos.width,carPos.step);
-        console.log(elementChildren[1])
-        console.log(rect.right);
+    
+    
+    
           
     });//end on event listener
     
