4a0d29108b2e408b76ee0624d96930750f0b194a
User <sr0876392@otc.edu>
2018-10-13 16:59:11 -0500

    still doesnt work

--- Midterm_Project_JS/script.js
+++ Midterm_Project_JS/script.js
@@ -15,26 +15,48 @@ const lights = {
 function fillCarsDiv() {
     for (i = 0; i < cars.length; i++){
         cars[i].element = document.createElement("div"); 
+        cars[i].element.setAttribute("id", `car${i+1}`)
         cars[i].element.innerHTML += cars[i].pic;
         carsDiv.appendChild(cars[i].element);
     }
 }
 
-function moveCars() {
-   var finishLinePos = document.getElementById("finishLine").getBoundingClientRect();
-   console.log(finishLinePos.right)
-  // i = 0;
-   //while (finishLinePos.right){
-   //  cars[i].element.style.marginLeft = "200px"
-    // i++
-  // }
-}
+/*function moveCars() {
+   var finishLineImg = document.getElementById("finishLine").getElementsByTagName("img");
+   var finishLinePos = finishLineImg[0].getBoundingClientRect().left;
+   cond = true;
+   rand = 0;
+   while(cond == true) {  
+            for (i = 1; i <= 3; i++){
+                
+                carpos = document.getElementById(`car${i}`).getElementsByTagName("img");
+                function funct(){
+                    rand += Math.floor(Math.random() * 10);
+                    carpos[0].style.marginLeft = `${rand}px`
+                    
+                }
+                setTimeout(funct(),1000);
+                
+                console.log(rand)
+                
+                if (finishLinePos < carpos[0].getBoundingClientRect().left){
+                cond = false
+                }
+            } 
+            i = 1;
+        }
+        
+    }
+   
+*/
+
 
 lights.startButton.addEventListener("click", function(){
     lights.redLight.style.backgroundColor = "grey";
     lights.greenLight.style.backgroundColor = "green";
-    cars.forEach(moveCars);
-   
+    cars[1].getBoundingClientRect().left
+    
+    
    
         
         
