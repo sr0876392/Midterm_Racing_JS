06a279909b275ca6fa7db5124fc037eeded2903a
User <sr0876392@otc.edu>
2018-10-13 23:24:52 -0500

    Cleaned up

--- Midterm_Project_JS/script_new.js
+++ Midterm_Project_JS/script_new.js
@@ -1,19 +1,20 @@
+//creating cars object
 const cars = {
+//creating cars array with 3 cars
    carsArr: ["<img id='greenCar' src='pics/green_car.png'> ",
              "<img id='yellowCar' src='pics/yellow_car.png'> ",
              "<img id='blackCar' src='pics/black_car.png'> "],
-
+//getting the carsDiv whic we will fill with cars in carsArr
    carsDiv: document.getElementById('cars'),
-
+//setting an value for car speed
+   carSpeed: 0,
+//fillCars fills the carsDiv with cars from carsArr
    fillCars : function createCarsDivs() {
         for (i = 0; i < this.carsArr.length; i++){
            this.carsDiv.innerHTML += this.carsArr[i]
         }
     },
-   // greenCarImg: document.getElementById('greenCar'),
-   // yellowCarImg: document.getElementById('yellowCar'),
-    //blackCarImg: document.getElementById('blackCar'),
-    
+//moveCars obtains all car images and their x coordinates 
     moveCars: function moveAllCars() {
         var finishLineImg = document.getElementById("fLineImg");
         finishLinePos = finishLineImg.getBoundingClientRect().left;
@@ -25,55 +26,37 @@ const cars = {
                         document.getElementById('yellowCar').getBoundingClientRect().left,
                         document.getElementById('blackCar').getBoundingClientRect().left,
         ];
-        
-        
-            console.log(carPositions[0], carPositions[1],carPositions[2])
-        
-            for (i = 0; i < carPositions.length; i++){
-                rand = Math.floor(Math.random() * 20);
-                carImg[i].style.marginLeft = `${carPositions[i] + rand}px`;
+// for loop sets margins for cars and updates their x coordinates
+        for (i = 0; i < carPositions.length; i++){
+                this.carSpeed = Math.floor(Math.random() * 15)
+                carImg[i].style.marginLeft = `${carPositions[i] + this.carSpeed}px`;
                 carPositions[i] = carPositions[i];
-                console.log('FOR LOOP')
-            }
-           
-
-        //console.log(finishLinePos,carPositions[0],document.getElementById('greenCar').getBoundingClientRect(), carPositions[1],carPositions[2], carImg[0])
+        };
     }
 }  
 
 
+//setting up the traffic light
 const lights = {
     greenLight: document.getElementById("green"),
     redLight: document.getElementById("red"),
     startButton: document.getElementById("start")
 };
-
+//runs fillCars on load
 cars.fillCars();
+//event listener to change the light color and start the race when start button is pressed
 lights.startButton.addEventListener("click", function(){
     lights.redLight.style.backgroundColor = "grey";
     lights.greenLight.style.backgroundColor = "green";
+    //calling moveCars and intervals in which moveCars will be called in runCars
     cars.moveCars()
     function runCars(){
-        for (i=0; carPositions[i] < (finishLinePos + 20); i++){
+        for (i=0; carPositions[i] < (finishLinePos + 30);){
             setTimeout(runCars, 32);
             cars.moveCars()
-        }
-        if (i == carPositions.length){
-            i = 0 ; 
-        }      
-                    
-            
-}
-runCars();
-            
-            
-            
-        
-  
-    
-    
-     
-      
+        };
+    }
+    runCars();
 });
 
 
